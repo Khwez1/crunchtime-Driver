927a2066b66774d7e6a9100ad8d8a16ee6876bdc
Sihle Mbobo <khwezimbobo@gmail.com>
2024-07-29 11:44:22 +0200

    map completed

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,42 @@
+import { Tabs } from 'expo-router';
+
+import { TabBarIcon } from '../../components/TabBarIcon';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'red',
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: 'History',
+          tabBarIcon: ({ color }) => <TabBarIcon name="clock-o" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="liked"
+        options={{
+          title: 'Liked',
+          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user-circle" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
